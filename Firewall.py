7795f999e71e1a0399ed8905cf394eaf4d4d7ec5
sahilgupta221 <sahil15057@iiitd.ac.in>
2018-05-16 20:20:13 -0400

    fields.append is corrected from rules.appended

--- Firewall.py
+++ Firewall.py
@@ -5,4 +5,4 @@ class Firewall:
     def __init__(self, number_of_rules,number_of_fields):
         self.rules=[]
         for i in range(0, number_of_rules):
-            self.fields.append(Rule(number_of_fields,randint(0, 2)))
+            self.rules.append(Rule(number_of_fields,randint(0, 2)))
