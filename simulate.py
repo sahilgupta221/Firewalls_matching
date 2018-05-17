b638d09fe81bcc5d52ac70038548fedebdf795b8
sahilgupta221 <sahil15057@iiitd.ac.in>
2018-05-16 21:00:02 -0400

    abstract code to compare two firewall is added.

--- simulate.py
+++ simulate.py
@@ -1,7 +1,17 @@
 from Firewall import *
-import sys
-
-import numpy as np
+from utils import*
 
 F1 = Firewall(10,5)
 F2 = Firewall(10,5)
+
+flag=1
+
+for rule in F2.rules:
+    p=Property(rule)
+    if (satisfy(F1,p)==0):
+        flag=0
+
+if(flag==0):
+    print("F1 and F2 are not same")
+else:
+    print("F1 and F2 are same")
