9cc1eb05d64337201c8862d6c692943957c0a664
sahilgupta221 <sahil15057@iiitd.ac.in>
2018-05-18 15:35:05 -0400

    Wip index implemented

--- Rule.py
+++ Rule.py
@@ -4,7 +4,17 @@ class Rule:
     def __init__(self, number_of_fields,action):
         self.fields=[]
         for i in range(0, number_of_fields):
-            self.fields.append([randint(0, 1000),randint(0, 1000)])
+            p=randint(0,100)
+            if(p<40):
+                self.fields.append([randint(0, 500),randint(300, 1000)])
+            elif(p>=40 and p<=60):
+                self.fields.append([0, 1000])
+            else:
+                single_value = randint(0,1000)
+                self.fields.append([single_value,single_value])
+
+
+
 
         self.fields.append(action)
 
