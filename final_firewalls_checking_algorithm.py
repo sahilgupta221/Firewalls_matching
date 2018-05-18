77f937d9217919b6bbe2352c4e9bd2b015b903d2
sahilgupta221 <sahil15057@iiitd.ac.in>
2018-05-18 13:28:55 -0400

    Final program made in final_firewalls_checking_algorithm file. utils functions updated. simulate is just temperary code having initial presentation program.

--- final_firewalls_checking_algorithm.py
+++ final_firewalls_checking_algorithm.py
@@ -2,48 +2,25 @@ from time import *
 from Firewall import *
 from utils import*
 
-
-start = clock_gettime(0)
-
-F1 = Firewall(10,5)
-F2 = Firewall(10,5)
-
-flag=1
-
-for rule in F2.rules:
-    p=Property(rule)
-    if (satisfy(F1,p)==0):
-        flag=0
-
-if(flag==0):
-    print("F1 and F2 are not same")
-else:
-    print("F1 and F2 are same")
-
-stop = clock_gettime(0)
-
-print("Time taken for comparision is ",stop-start)
-
-
 """
 1. Take two firewalls, F and G.
 Now, we want to find a witness packet accepted by F and discarded by G.
 (or vice versa, in which case, switch the names F and G.)
 
-2. Make all accept slices of F, call them F1, F2 … 
-Make all discard slices of G, call them G1, G2 … 
+2. Make all accept slices of F, call them F1, F2 …
+Make all discard slices of G, call them G1, G2 …
 
 3. Take an accept slice Fa and a discard slice Gb.
 Project Fa on base rule of Gb, to get Fab.
 Project Gb on base rule of Fa, to get Gba.
 
-4. Flip decisions for Gba. 
+4. Flip decisions for Gba.
 (After flipping, the last rule is now an accept and the rules masking it are discards).
 
 Note that now, Fab and Gba both end in the exact same rule.
 (Predicate: intersection of base rules of Fa and Gb. Decision: accept.)
 
-6. Make one slice with all the rules from Fab (except the base rule), followed by 
+6. Make one slice with all the rules from Fab (except the base rule), followed by
 all the rules from Gba (this time, including the base rule).
 
 Now, the only packets accepted by this slice are those that :
--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 from Firewall import *
 from itertools import product
-from Property import *
+import copy
 
 def satisfy(Frwl,p):
     #list of slice firewall.
@@ -33,7 +33,7 @@ def satisfy(Frwl,p):
 # loop over each slice firewall and use prob algorithm to get witness packet over that slice
         for firewall in slice_firewall:
             temp_firewall = projection(firewall,p)
-            f= probe_field_set(temp_firewall,p,friend_action_index)
+            f= probe_field_set(temp_firewall,p,enemy_action_index)
 
 # collect all packet in Packet list
         packet_list = fill_possible_packet(f)
@@ -90,20 +90,6 @@ def projection(frwl,p):
 
     return Firewall(rule_list)
 
-def projection_from_base_rule(f,g):
-    return
-
-def flip_actions(g):
-    return
-
-def intersection_of_base_rules(f,g):
-    return
-
-def merge_two_firewall_slice(f,g):
-    return
-
-def packets_from_probe_algorithm(f,g):
-    return
 
 # probe algorithm to get possible values at each field positions
 # remember to remove elements that are out of property range generated from the process
@@ -130,5 +116,110 @@ def probe_field_set(frwl,p,friend_action_index):
 def fill_possible_packet(field_list):
     return list(product(*field_list))
 
+
+###################################################################################
+
 def create_slice_firewall(firewall,split_action):
-    return None
+    slice_firewall = []
+
+    # created enemy and friend rule index as per property p
+    # action index contain friend(1) and enemy(0) value same position as of rules
+    friend_action_index = []
+    i = 0
+    for rule in firewall.rules:
+        if (rule.fields[firewall.number_of_fields] == split_action):
+            friend_action_index.append(i)
+        i = i + 1
+
+        # all enemy rules index and friend rules above it having some over lap is clubbed together
+        # to form a slice
+    for index in friend_action_index:
+        rules = []
+        for i in range(0, index + 1):
+            if (i in friend_action_index and i != index):
+                continue
+            if (is_overlap(firewall.rules[i], firewall.rules[index])):
+                rules.append(firewall.rules[i])
+        slice_firewall.append(Firewall(rules))
+
+    return slice_firewall
+
+
+def is_overlap(firewall_rule,base_rule):
+    for i in range(len(firewall_rule.fields)):
+        if(base_rule.fields[i][0]<firewall_rule.fields[i][0] and base_rule.fields[i][1]<firewall_rule.fields[i][0]):
+            return False
+        if(base_rule.fields[i][0]>firewall_rule.fields[i][1] and base_rule.fields[i][1]>firewall_rule.fields[i][1]):
+            return False
+        if(base_rule.fields[i][0]>firewall_rule.fields[i][0] and base_rule.fields[i][1]<firewall_rule.fields[i][1]):
+            return True
+        if(base_rule.fields[i][0]<firewall_rule.fields[i][0] and base_rule.fields[i][1]>firewall_rule.fields[i][0]):
+            return True
+        if(base_rule.fields[i][0]<firewall_rule.fields[i][1] and base_rule.fields[i][1]>firewall_rule.fields[i][1]):
+            return True
+    return True
+
+def re_adjust_ranges_as_per_intersections(firewall_rule,base_rule):
+    fields = []
+
+    for i in range(len(firewall_rule.fields)):
+        if(base_rule.fields[i][0]<firewall_rule.fields[i][0] and base_rule.fields[i][1]<firewall_rule.fields[i][0]):
+            return Rule([])
+        if(base_rule.fields[i][0]>firewall_rule.fields[i][1] and base_rule.fields[i][1]>firewall_rule.fields[i][1]):
+            return Rule([])
+        if(base_rule.fields[i][0]>firewall_rule.fields[i][0] and base_rule.fields[i][1]<firewall_rule.fields[i][1]):
+            fields.append([base_rule.fields[i][0],base_rule.fields[i][1]])
+        if(base_rule.fields[i][0]<firewall_rule.fields[i][0] and base_rule.fields[i][1]>firewall_rule.fields[i][0]):
+            fields.append([firewall_rule.fields[i][0],base_rule.fields[i][1]])
+        if(base_rule.fields[i][0]<firewall_rule.fields[i][1] and base_rule.fields[i][1] > firewall_rule.fields[i][1]):
+            fields.append([base_rule.fields[i][0],firewall_rule.fields[i][1]])
+
+    return Rule(fields)
+
+def projection_from_base_rule(f,g):
+    rule_list = []
+    for i in range(len(f.rules)):
+        rule_list.append(re_adjust_ranges_as_per_intersections(f.rules[i],g.rules[len(g)-1]))
+
+    return Firewall(rule_list)
+
+def flip_actions(g):
+    rule_list = []
+    for i in range(len(g.rules)):
+        temp = copy.deepcopy(g.rules[i])
+        temp.rules.fields[len(temp.rules.fields)]=0 if temp.rules.fields[len(temp.rules.fields)]==1 else 1
+        rule_list.append(temp)
+    return Firewall(rule_list)
+
+def intersection_of_base_rules(f,g):
+    return []
+
+def merge_two_firewall_slice(f,g):
+    f_new = projection_from_base_rule(f, f)
+    g_new = projection_from_base_rule(g, g)
+    f_new.rules.remove(f_new.rules[len(f_new.rules)-1])
+    rules = f_new.rules+g_new.rules
+    merged_firewall = Firewall(rules)
+    return merged_firewall
+
+def packets_from_probe_algorithm(firewall):
+    base_rule=firewall.rules[len(firewall.rules)]
+
+    # f is list of possible edge points of various fields like f[0] contain first field edge values
+    f=[]
+    for rule in firewall.rules:
+        if rule.fields[len(rule.fields)-1] == base_rule.fields[len(base_rule.fields)-1]:
+            j=0
+            for field in rule.fields:
+                if(field[1] + 1<=base_rule.fields[1]):
+                    f[j].append(field[1] + 1)
+                j = j + 1
+        else:
+            k=0
+            for field in rule.fields:
+                f[k].append(field[0])
+                k=k+1
+
+    return f
+
+#############################################################################
